ab44306847c4e93a2ec5da0501ab6c529f9d9243
shreyawagh343 <shreyawagh343@gmail.com>
2025-02-17 20:13:36 +0530

    Working on client side

---
+++ Frontend/Frontend/src/Components/Ideas.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Ideas = () => {
+  return (
+    <>
+    <div className="bg-">
+        <div className="">
+
+        </div>
+        <div className="">
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Ideas
