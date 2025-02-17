7b4a4fd43915ada995f28f2c314551f5b555c1c2
shreyawagh343 <shreyawagh343@gmail.com>
2025-02-17 20:06:41 +0530

    Working on client side

--- Frontend/Frontend/src/Components/Navabar.jsx
+++ Frontend/Frontend/src/Components/Navabar.jsx
@@ -1,28 +1,67 @@
-import React from 'react'
-import logo from "../../public/logo.png"
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa"; 
+import logo from "../../public/Images/Main Logo.png";
 
-const Navabar = () => {
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
-    <div className="flex justify-between">
-            <div className="logo flex">
-                <img src={logo} alt="" className='h-20 w-20 mt-3'/>
-                <h1 className='-ml-3 mt-9 text-2xl text-red-700'>PicSearch</h1>
-                <h1 className='ml-5 mt-9 text-2xl cursor-pointer'>Explore</h1>
-            </div>
-            <div className="Navbar mr-10">
-                <ul className='flex gap-6 mt-7 text-[1.3rem]'>
-                    <li className='hover:text-red-500 cursor-pointer hover:underline mt-2'>Home</li>
-                    <li className='hover:text-red-500 cursor-pointer hover:underline mt-2'>Bussiness</li>
-                    <li className='hover:text-red-500 cursor-pointer hover:underline mt-2'>Blog</li>
-                    <li><button className="bg-red-500 text-white  p-1.5 pl-4 pr-4 rounded-md text-xl mt-1">Sign Up</button>
-                    </li>
-                    <li><button className="bg-red-500 text-white  p-1.5 pl-4 pr-4 rounded-md text-xl mt-1">Log In</button></li>
-                </ul>
-            </div>
+    <nav className="w-full bg-white">
+      <div className="flex justify-between mr-4 ">
+        <div className="flex">
+          <img
+            src={logo}
+            alt="Logo"
+            className="h-[15vh] w-[30vw] sm:h-[18vh] sm:w-[10vw]"
+          />
+          <h1 className="mt-8 text-xl sm:text-2xl font-bold cursor-pointer hover:text-red-500 md:mt-12 md:-ml-3">
+            Explore
+          </h1>
         </div>
-    </>
-  )
-}
+        <ul className="hidden md:mt-12 md:flex gap-6 text-lg">
+          <li className="hover:text-red-500 cursor-pointer">Home</li>
+          <li className="hover:text-red-500 cursor-pointer">Business</li>
+          <li className="hover:text-red-500 cursor-pointer">Blog</li>
+          <li>
+            <button className="bg-red-500 text-white px-4 py-2 rounded-md">
+              Sign Up
+            </button>
+          </li>
+          <li>
+            <button className="bg-red-500 text-white px-4 py-2 rounded-md">
+              Log In
+            </button>
+          </li>
+        </ul>
 
-export default Navabar
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="md:hidden text-2xl "
+        >
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {menuOpen && (
+        <ul className="md:hidden flex flex-col items-center gap-4 bg-white shadow-md py-4">
+          <li className="hover:text-red-500 cursor-pointer">Home</li>
+          <li className="hover:text-red-500 cursor-pointer">Business</li>
+          <li className="hover:text-red-500 cursor-pointer">Blog</li>
+          <li>
+            <button className="bg-red-500 text-white px-6 py-2 rounded-md">
+              Sign Up
+            </button>
+          </li>
+          <li>
+            <button className="bg-red-500 text-white px-6 py-2 rounded-md">
+              Log In
+            </button>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
